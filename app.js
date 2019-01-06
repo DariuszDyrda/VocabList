5f6269a704909b4542ce9bc335494cd1b292a93f
Dariusz Dyrda <darek.d.az.nie@interia.pl>
2019-01-06 04:19:05 +0100

    add views files, move routes to its own file

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const listsRouter = require('./routes/lists');
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Home page!');
-});
+app.set('view engine', 'ejs');
+app.use('/', listsRouter);
 
 app.listen(3000, () => {
   console.log('The server has started!');
