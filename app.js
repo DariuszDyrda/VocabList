97d85db7f3a88611ecc726dfe022bc1c37a22808
Dariusz Dyrda <darek.d.az.nie@interia.pl>
2019-01-09 19:05:03 +0100

    add register and login functionality

--- app.js
+++ app.js
@@ -2,16 +2,41 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require("express-session");
+
 const listsRouter = require('./routes/lists');
 const wordsRouter = require('./routes/words');
 const List = require('./models/List');
+const User = require('./models/User');
 const seedDb = require('./seeds');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
 app.use(methodOverride('_method'));
+
+passport.use(new LocalStrategy(User.authenticate()));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function (req, res, next) {
+  res.locals = {
+    user: req.user
+  };
+  next();
+});
+
 app.use('/', listsRouter);
 app.use('/', wordsRouter);
 
--- routes/lists.js
+++ routes/lists.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const passport = require('passport');
 const List = require('../models/List');
 const Word = require('../models/Word');
+const User = require('../models/User');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('landing');
+});
+router.get('/register', function(req, res) {
+  res.render('register', { });
+});
+router.post('/register', function(req, res) {
+  User.register(new User({ username : req.body.username }), req.body.password, function(err, account) {
+      if (err) {
+          return res.render('register', { user : user });
+      }
+
+      passport.authenticate('local')(req, res, function () {
+          res.redirect('/index');
+      });
+  });
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', { user : req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/index');
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/index');
 });
 router.get('/index', (req, res) => {
   List.find({}, (err, lists) => {
