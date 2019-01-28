b055cfc1afda349ba84fd45eeca0b742d9e7b8e4
Dariusz Dyrda <darek.d.az.nie@interia.pl>
2019-01-28 02:06:42 +0100

    add edit dialog insted of form in the table

--- public/scripts/listShowPage.js
+++ public/scripts/listShowPage.js
@@ -1,6 +1,21 @@
-$(document).ready(function () {
-    $("table").on("click", "#editButton", function () {
-        $(this).parents("tr").next().toggleClass("hidden");
-        $(this).parents("tr").toggleClass("hidden");
+$( function() {
+    var word = null;
+    var translation = null;
+    var wordId = null;
+    var dialog = $( "#dialog-form" ).dialog({
+      autoOpen: false,
+      modal: true,
     });
-});
+ 
+
+    $("table").on("click", ".editButton", function () {
+        wordId = $(this).data('wordid');
+        word = $(this).parents("tr").find("span").first().text();
+        translation = $(this).parents("tr").find("span").eq(1).text();
+        let action = $('#editForm').attr('action');
+         $('#editForm').attr('action',  action + `${wordId}?_method=PUT`);
+        $("#word").val(word);
+        $("#translation").val(translation);
+        dialog.dialog( "open" );
+    });
+  } );
